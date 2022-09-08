3dd376bff27031318567b2c9cd2613fcdb8f8d7e
ThaisFrancaI <thais.inacio@sptech.school>
2022-09-08 13:09:27 -0300

    Codigo Kotlin atualizado

--- Thais/DesafioKotlin/src/main/kotlin/DesafioKotlin/CriarUsuario.kt
+++ Thais/DesafioKotlin/src/main/kotlin/DesafioKotlin/CriarUsuario.kt
@@ -1,29 +1,15 @@
 package DesafioKotlin
 
+import javax.swing.JOptionPane
+
+
 class CriarUsuario {
+    lateinit var nome:String
     lateinit var email:String
     lateinit var senha:String
 
-    // criando um lista de String vazia que permite
-    // entrada/saída/atualização de itens
-    val emailsUsados = mutableListOf<String>()
-    val senhasUsadas = mutableListOf<String>()
-
-    fun validarEmail(email:String) {
-        emailsUsados.add(email)
+    fun validar(emailLogin:String, senhaLogin:String):Boolean {
+        return email == emailLogin && senha == senhaLogin
     }
-
-    fun validarSenha(senha:String) {
-        senhasUsadas.add(senha)
-    }
-
-    fun cadastrar():String{
-        var frase = "Cadastro realizado com sucesso!Bem vindo"
-        emailsUsados.forEach{
-            frase += "- $it \r\n"
-        }
-        senhasUsadas.forEach{
-        }
-        return frase
-    }
+//
 }
--- Thais/DesafioKotlin/src/main/kotlin/DesafioKotlin/appUsuario.kt
+++ Thais/DesafioKotlin/src/main/kotlin/DesafioKotlin/appUsuario.kt
@@ -3,12 +3,32 @@ package DesafioKotlin
 import javax.swing.JOptionPane
 
 fun main() {
-    val usuario1 = CriarUsuario()
-    usuario1.email = JOptionPane.showInputDialog("Digite seu email")
-    usuario1.senha = JOptionPane.showInputDialog("Digite sua senha")
+    val usuarios = mutableListOf<CriarUsuario>()
 
-    usuario1.validarEmail(usuario1.email)
-    usuario1.validarSenha(usuario1.senha)
+    while (true){
+        val opcao = JOptionPane.showInputDialog("Digite 1 para cadastro ou 2 para login").toInt()
 
-    JOptionPane.showMessageDialog(null, usuario1.cadastrar())
+        if(opcao == 1){
+            val usuario = CriarUsuario()
+            usuario.nome = JOptionPane.showInputDialog("Digite seu nome completo")
+            usuario.email = JOptionPane.showInputDialog("Digite seu email")
+            usuario.senha = JOptionPane.showInputDialog("Digite sua senha")
+            usuarios.add(usuario)
+            JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!")
+        }
+
+        if (opcao == 2){
+            var email = JOptionPane.showInputDialog("Digite seu email")
+            var senha = JOptionPane.showInputDialog("Digite sua senha")
+
+            usuarios.forEach { usuarioAtual ->
+                if (usuarioAtual.validar(email, senha)){
+                    JOptionPane.showMessageDialog(null, "Logado com sucesso!")
+                }else {
+                    JOptionPane.showMessageDialog(null, "Login inválido")
+                }
+            }
+        }
+    }
+
 }
