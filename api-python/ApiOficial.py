92639c2332b4ed2007479e5314340a4f8ce01de9
joaqu1m <joaquim.pires@sptech.school>
2022-09-28 14:10:19 -0300

    PYTHON ARRUMADO CARALHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- api-python/ApiOficial.py
+++ api-python/ApiOficial.py
@@ -1,86 +1,60 @@
-from datetime import datetime
 import psutil
 import mysql.connector
-import time 
 from mysql.connector import errorcode
 
-# i = 0
+i = 0
 while True:
-    # i = i + 1
+    i += 1
 
     try:
-        db_connection = mysql.connector.connect(
-            host='localhost', user='root', password='#Gf40928326802', database='ekran')
-        print("Conectei no banco!")
+        db_connection = mysql.connector.connect(host='localhost', user='aluno', password='sptech', database='ekran')
     except mysql.connector.Error as error:
         if error.errno == errorcode.ER_BAD_DB_ERROR:
-             print("Não encontrei o banco")
+            print("Não encontrei o banco")
         elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-           print("Credenciais erradas")
+            print("Credenciais erradas")
         else:
-           print(error)
+            print(error)
 
     # Processador
-    processador = psutil.cpu_count()
-    porcentagem_cpu = psutil.cpu_percent()
-    porcentagem_cpu2 = porcentagem_cpu * 1.10
-    porcentagem_cpu3 = porcentagem_cpu * 0.95
+    arrayAtual = psutil.cpu_percent(interval=3, percpu=True)
+    psutilCpuPercent = 0
+    f = 0
+    while (f < len(arrayAtual)):
+        psutilCpuPercent += arrayAtual[f]
+        f += 1
+    psutilCpuPercent /= len(arrayAtual)
+    cpuPercent = ["%0.2f" % (psutilCpuPercent), "%0.2f" % (psutilCpuPercent * 1.10), "%0.2f" % (psutilCpuPercent * 0.95)]
 
     # Disco
-    discoTotal =(psutil.disk_usage("C:\\")[0] / 10**9)
-    discoUso = (psutil.disk_usage("C:\\")[1] / 10**9)
-    discoUso2 = discoUso * 0.95
-    discoUso3 = discoTotal
-    discoLivre = (psutil.disk_usage("C:\\")[2] / 10**9)
-    discoLivre2 = discoTotal - discoUso2
-    discoLivre3 = discoTotal - discoUso3
-    disk = psutil.disk_usage('C:\\')
-    diskPercent = disk.percent
-    diskPercent2 = diskPercent * 0.95
-    diskPercent3 = 100
+    psutilDiskPercent = psutil.disk_usage('C:\\')[3]
+    diskPercent = ["%0.2f" % (psutilDiskPercent), "%0.2f" % (psutilDiskPercent * 0.95), "%0.2f" % (psutilDiskPercent * 1.05)]
 
     # Ram
-    ramTotal = (psutil.virtual_memory() [0] / 10**9)
-    ramUso =  (psutil.virtual_memory() [3] / 10**9)
-    ramUso2 = ramUso * 1.15
-    ramUso3 = ramUso2 * 1.05
-    # ramUsoPercent = "%0.2f" % (psutil.virtual_memory() [2])
-    ram = (psutil.virtual_memory())
-    ramPercent = ram.percent
-    ramPercent2 = ramPercent * 1.15
-    ramPercent3 = ramPercent2 * 1.05
+    psutilRamPercent = psutil.virtual_memory()[2]
+    ramPercent = ["%0.2f" % (psutilRamPercent), "%0.2f" % (psutilRamPercent * 1.15), "%0.2f" % (psutilRamPercent * 1.05)]
 
-
-    # Internet 
-    pctEnv = (psutil.net_io_counters() [2] / 1024)
-    pctEnv2 = pctEnv + 7
-    pctEnv3 = pctEnv - 9
-    pctRecv = (psutil.net_io_counters() [3]/ 1024)
-    pctRecv2 = pctRecv + 7
-    pctRecv3 = pctRecv - 9
+    # Internet Upload
+    psutilMbUpload = psutil.net_io_counters()[0] * 10**-6
+    psutilMbDownload = psutil.net_io_counters()[1] * 10**-6
+    mbUpload = ["%0.3f" % (psutilMbUpload), "%0.3f" % (psutilMbUpload * 1.1), "%0.3f" % (psutilMbUpload * 0.95)]
+    mbDownload = ["%0.3f" % (psutilMbDownload), "%0.3f" % (psutilMbDownload * 1.1), "%0.3f" % (psutilMbDownload * 0.95)]
     
+    print("------------------------------------------")
 
-
-    db_connection = mysql.connector.connect(host="localhost", user="root", passwd="#Gf40928326802", database="ekran")
     cursor = db_connection.cursor()
     fkTotem = 50000
-    sql = "INSERT INTO Leitura (fkTotem, CPUM, qtdProcessador, ramTotal, ramUso,  ramUsoPercent, discoTotal, discoUso, discoLivre, discoPercent, qtdPacoteEnv, qtdPacoteRecv, dataHora) VALUES (%s,%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s, (SELECT NOW()))"
-    values = [fkTotem, porcentagem_cpu, processador, ramTotal, ramUso, ram.percent, discoTotal, discoUso, discoLivre, disk.percent, pctEnv, pctRecv]
-    cursor.execute(sql, values)
-    
-    fkTotem = 50001
-    sql = "INSERT INTO Leitura (fkTotem, CPUM, qtdProcessador, ramTotal, ramUso,  ramUsoPercent, discoTotal, discoUso, discoLivre, discoPercent, qtdPacoteEnv, qtdPacoteRecv, dataHora) VALUES (%s,%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s, (SELECT NOW()))"
-    values = [fkTotem, porcentagem_cpu2, processador, ramTotal, ramUso2, ramPercent2, discoTotal, discoUso2, discoLivre2, diskPercent2, pctEnv2, pctRecv2]
-    cursor.execute(sql, values)
+    f = 0
+    while f < 3:
+        sql = "INSERT INTO Leitura (fkTotem, cpuPercent, diskPercent, ramPercent, mbUpload, mbDownload) VALUES (%s,%s,%s,%s,%s,%s)"
+        values = [fkTotem, float(cpuPercent[f]), float(diskPercent[f]), float(ramPercent[f]), float(mbUpload[f]), float(mbDownload[f])]
+        print(values)
+        cursor.execute(sql, values)
+        fkTotem += 1
+        f += 1
 
-    fkTotem = 50002
-    sql = "INSERT INTO Leitura (fkTotem, CPUM, qtdProcessador, ramTotal, ramUso,  ramUsoPercent, discoTotal, discoUso, discoLivre, discoPercent, qtdPacoteEnv, qtdPacoteRecv, dataHora) VALUES (%s,%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s, (SELECT NOW()))"
-    values = [fkTotem, porcentagem_cpu3, processador, ramTotal, ramUso3, ramPercent3, discoTotal, discoUso3, discoLivre3, diskPercent3, pctEnv3, pctRecv3]
-    cursor.execute(sql, values)
-
-
-    print("\n")
-    print(cursor.rowcount, "Inserindo no banco.")
+    print(i, "Inserindo no banco.")
+    print("------------------------------------------")
+    print("\r")
     db_connection.commit()
-    time.sleep(5)
-    
+    db_connection.close()
